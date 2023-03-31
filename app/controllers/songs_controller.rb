05291c69acf7ccbc89175af376ae46adecf819b3
Tom Gagliano <tgaglianoll@gmail.com>
2023-03-31 13:56:14 -0400

    Add create action

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,4 +9,16 @@ class SongsController < ApplicationController
     @songs = Song.all
     render :index
   end
+
+  def create
+    @song = Song.new(
+    title: params[:title],
+    album: params[:album],
+    artist: params[:artist],
+    year: params[:year]
+  )
+    @song.save
+    render :show
+  end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
  get "/songs/:id" => "songs#show"
  get "/songs" => "songs#index"
+ post "/songs" => "songs#create"
 end
